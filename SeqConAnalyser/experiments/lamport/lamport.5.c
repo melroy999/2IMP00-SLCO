121f957a32590f017efe8975fe4c7cdbbeb75813
awijs <A.J.Wijs@tue.nl>
2020-06-17 12:26:07 +0200

    Fixed Lamport models

--- SeqConAnalyser/experiments/lamport/lamport.5.c
+++ SeqConAnalyser/experiments/lamport/lamport.5.c
@@ -3,147 +3,255 @@ int y;
 int b1, b2, b3, b4, b5, b6; // N boolean flags
 
 void* thr1(void * arg) {
-  L0:
-    b1 = 1;
-    x = 1;
-    if (y != 0) {
-      b1 = 0;
-      __CPROVER_assume(y == 0);
-      goto L0;
-    }
-    y = 1;
-    if (x != 1) {
-      b1 = 0;
-      __CPROVER_assume (b2 == 0 && b3 == 0 && b4 == 0 && b5 == 0 && b6 == 0);
-      if (y != 1) {
-	__CPROVER_assume (y == 0);
-	goto L0;
-      }
-    }
-  // begin: critical section
-  // end: critical section
-  y = 0;
-  b1 = 0;
+	while(1) {
+		// SL0
+		b1 = 1;
+		// SimpleState0
+		x = 1;
+		// S0
+		if (y != 0) {
+			// SimpleState1
+			b1 = 0;
+			continue;
+			// -> SL0
+		}
+		else {
+			y = 1;
+			// S1
+			if (x == 1) {
+				// S3
+				y = 0;
+				// SimpleState2
+			SimpleState2:
+				b1 = 0;
+				break;
+				// -> STOP
+			}
+			else {
+				b1 = 0;
+				// S2
+				if (y != 1) {
+					continue;
+					// -> SL0
+				}
+				else {
+					y = 0;
+					// SimpleState2
+					goto SimpleState2;
+				}
+			}
+		}
+  }
 }
 
 void* thr2(void * arg) {
-  L1:
-    b2 = 1;
-    x = 2;
-    if (y != 0) {
-      b2 = 0;
-      __CPROVER_assume (y == 0);
-      goto L1;
-    }
-    y = 2;
-    if (x != 2) {
-      b2 = 0;
-      __CPROVER_assume (b1 == 0 && b3 == 0 && b4 == 0 && b5 == 0 && b6 == 0);
-      if (y != 2) {
-	__CPROVER_assume (y == 0);
-	goto L1;
-      }
-    }
-  // begin: critical section
-  // end: critical section
-  y = 0;
-  b2 = 0;
+	while(1) {
+		// SL0
+		b2 = 1;
+		// SimpleState0
+		x = 2;
+		// S0
+		if (y != 0) {
+			// SimpleState1
+			b2 = 0;
+			continue;
+			// -> SL0
+		}
+		else {
+			y = 2;
+			// S1
+			if (x == 2) {
+				// S3
+				y = 0;
+				// SimpleState2
+			SimpleState2:
+				b2 = 0;
+				break;
+				// -> STOP
+			}
+			else {
+				b2 = 0;
+				// S2
+				if (y != 2) {
+					continue;
+					// -> SL0
+				}
+				else {
+					y = 0;
+					// SimpleState2
+					goto SimpleState2;
+				}
+			}
+		}
+  }
 }
 
 void* thr3(void * arg) {
-  L2:
-    b3 = 1;
-    x = 3;
-    if (y != 0) {
-      b3 = 0;
-      __CPROVER_assume (y == 0);
-      goto L2;
-    }
-    y = 3;
-    if (x != 3) {
-      b3 = 0;
-      __CPROVER_assume (b1 == 0 && b2 == 0 && b4 == 0 && b5 == 0 && b6 == 0);
-      if (y != 3) {
-    __CPROVER_assume (y == 0);
-    goto L2;
-      }
-    }
-  // begin: critical section
-  // end: critical section
-  y = 0;
-  b3 = 0;
+	while(1) {
+		// SL0
+		b3 = 1;
+		// SimpleState0
+		x = 3;
+		// S0
+		if (y != 0) {
+			// SimpleState1
+			b3 = 0;
+			continue;
+			// -> SL0
+		}
+		else {
+			y = 3;
+			// S1
+			if (x == 3) {
+				// S3
+				y = 0;
+				// SimpleState2
+			SimpleState2:
+				b3 = 0;
+				break;
+				// -> STOP
+			}
+			else {
+				b3 = 0;
+				// S2
+				if (y != 3) {
+					continue;
+					// -> SL0
+				}
+				else {
+					y = 0;
+					// SimpleState2
+					goto SimpleState2;
+				}
+			}
+		}
+  }
 }
 
 void* thr4(void * arg) {
-  L3:
-    b4 = 1;
-    x = 4;
-    if (y != 0) {
-      b4 = 0;
-      __CPROVER_assume (y == 0);
-      goto L3;
-    }
-    y = 4;
-    if (x != 4) {
-      b4 = 0;
-      __CPROVER_assume (b1 == 0 && b2 == 0 && b3 == 0 && b5 == 0 && b6 == 0);
-      if (y != 4) {
-    __CPROVER_assume (y == 0);
-    goto L3;
-      }
-    }
-  // begin: critical section
-  // end: critical section
-  y = 0;
-  b4 = 0;
+	while(1) {
+		// SL0
+		b4 = 1;
+		// SimpleState0
+		x = 4;
+		// S0
+		if (y != 0) {
+			// SimpleState1
+			b4 = 0;
+			continue;
+			// -> SL0
+		}
+		else {
+			y = 4;
+			// S1
+			if (x == 4) {
+				// S3
+				y = 0;
+				// SimpleState2
+			SimpleState2:
+				b4 = 0;
+				break;
+				// -> STOP
+			}
+			else {
+				b4 = 0;
+				// S2
+				if (y != 4) {
+					continue;
+					// -> SL0
+				}
+				else {
+					y = 0;
+					// SimpleState2
+					goto SimpleState2;
+				}
+			}
+		}
+  }
 }
 
 void* thr5(void * arg) {
-  L4:
-    b5 = 1;
-    x = 5;
-    if (y != 0) {
-      b5 = 0;
-      __CPROVER_assume (y == 0);
-      goto L4;
-    }
-    y = 5;
-    if (x != 5) {
-      b5 = 0;
-      __CPROVER_assume (b1 == 0 && b2 == 0 && b3 == 0 && b4 == 0 && b6 == 0);
-      if (y != 5) {
-    __CPROVER_assume (y == 0);
-    goto L4;
-      }
-    }
-  // begin: critical section
-  // end: critical section
-  y = 0;
-  b5 = 0;
+	while(1) {
+		// SL0
+		b5 = 1;
+		// SimpleState0
+		x = 5;
+		// S0
+		if (y != 0) {
+			// SimpleState1
+			b5 = 0;
+			continue;
+			// -> SL0
+		}
+		else {
+			y = 5;
+			// S1
+			if (x == 5) {
+				// S3
+				y = 0;
+				// SimpleState2
+			SimpleState2:
+				b5 = 0;
+				break;
+				// -> STOP
+			}
+			else {
+				b5 = 0;
+				// S2
+				if (y != 5) {
+					continue;
+					// -> SL0
+				}
+				else {
+					y = 0;
+					// SimpleState2
+					goto SimpleState2;
+				}
+			}
+		}
+  }
 }
 
 void* thr6(void * arg) {
-  L5:
-    b6 = 1;
-    x = 6;
-    if (y != 0) {
-      b6 = 0;
-      __CPROVER_assume (y == 0);
-      goto L5;
-    }
-    y = 6;
-    if (x != 6) {
-      b6 = 0;
-      __CPROVER_assume (b1 == 0 && b2 == 0 && b3 == 0 && b4 == 0 && b5 == 0);
-      if (y != 6) {
-    __CPROVER_assume (y == 0);
-    goto L5;
-      }
-    }
-  // begin: critical section
-  // end: critical section
-  y = 0;
-  b6 = 0;
+	while(1) {
+		// SL0
+		b6 = 1;
+		// SimpleState0
+		x = 6;
+		// S0
+		if (y != 0) {
+			// SimpleState1
+			b6 = 0;
+			continue;
+			// -> SL0
+		}
+		else {
+			y = 6;
+			// S1
+			if (x == 6) {
+				// S3
+				y = 0;
+				// SimpleState2
+			SimpleState2:
+				b6 = 0;
+				break;
+				// -> STOP
+			}
+			else {
+				b6 = 0;
+				// S2
+				if (y != 6) {
+					continue;
+					// -> SL0
+				}
+				else {
+					y = 0;
+					// SimpleState2
+					goto SimpleState2;
+				}
+			}
+		}
+  }
 }
 
 int main()
