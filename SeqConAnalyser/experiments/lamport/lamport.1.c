869dc718c9ff0038d09fdc85a1417a79ed28c1da
awijs <A.J.Wijs@tue.nl>
2020-06-17 12:16:05 +0200

    Update

--- SeqConAnalyser/experiments/lamport/lamport.1.c
+++ SeqConAnalyser/experiments/lamport/lamport.1.c
@@ -3,51 +3,87 @@ int y;
 int b1, b2; // N boolean flags
 
 void* thr1(void * arg) {
-  L0:
-    b1 = 1;
-    x = 1;
-    if (y != 0) {
-      b1 = 0;
-      __CPROVER_assume(y == 0);
-      goto L0;
-    }
-    y = 1;
-    if (x != 1) {
-      b1 = 0;
-      __CPROVER_assume (b2 == 0);
-      if (y != 1) {
-	__CPROVER_assume (y == 0);
-	goto L0;
-      }
-    }
-  // begin: critical section
-  // end: critical section
-  y = 0;
-  b1 = 0;
+	while(1) {
+		// SL0
+		b1 = 1;
+		// SimpleState0
+		x = 1;
+		// S0
+		if (y != 0) {
+			// SimpleState1
+			b1 = 0;
+			continue;
+			// -> SL0
+		}
+		else {
+			y = 1;
+			// S1
+			if (x == 1) {
+				// S3
+				y = 0;
+				// SimpleState2
+			SimpleState2:
+				b1 = 0;
+				break;
+				// -> STOP
+			}
+			else {
+				b1 = 0;
+				// S2
+				if (y != 1) {
+					continue;
+					// -> SL0
+				}
+				else {
+					y = 0;
+					// SimpleState2
+					goto SimpleState2
+				}
+			}
+		}
+  }
 }
 
 void* thr2(void * arg) {
-  L1:
-    b2 = 1;
-    x = 2;
-    if (y != 0) {
-      b2 = 0;
-      __CPROVER_assume (y == 0);
-      goto L1;
-    }
-    y = 2;
-    if (x != 2) {
-      b2 = 0;
-      __CPROVER_assume (b1 == 0);
-      if (y != 2) {
-	__CPROVER_assume (y == 0);
-	goto L1;
-      }
-    }
-  // begin: critical section
-  // end: critical section
-  y = 0;
-  b2 = 0;
+	while(1) {
+		// SL0
+		b2 = 1;
+		// SimpleState0
+		x = 2;
+		// S0
+		if (y != 0) {
+			// SimpleState1
+			b2 = 0;
+			continue;
+			// -> SL0
+		}
+		else {
+			y = 2;
+			// S1
+			if (x == 2) {
+				// S3
+				y = 0;
+				// SimpleState2
+			SimpleState2:
+				b2 = 0;
+				break;
+				// -> STOP
+			}
+			else {
+				b2 = 0;
+				// S2
+				if (y != 2) {
+					continue;
+					// -> SL0
+				}
+				else {
+					y = 0;
+					// SimpleState2
+					goto SimpleState2
+				}
+			}
+		}
+  }
 }
 
 int main()
