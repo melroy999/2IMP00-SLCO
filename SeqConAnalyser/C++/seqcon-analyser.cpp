124bf8f001bd9dfdcb4411496a339889e18bb5c8
awijs <A.J.Wijs@tue.nl>
2020-06-22 14:17:00 +0200

    Fix

--- SeqConAnalyser/C++/seqcon-analyser.cpp
+++ SeqConAnalyser/C++/seqcon-analyser.cpp
@@ -2485,9 +2485,9 @@ int main (int argc, char *argv[]) {
 				// 	mark[marked_stack.peek()] = false;
 				// 	marked_stack.pop();
 				// }
+				visited_locs[sa.location] = false;
+				visited_threads[sa.tid] = false;
 			}
-			visited_locs[sa.location] = false;
-			visited_threads[sa.tid] = false;
 		}
 		// cout << "Marked for fencing:" << endl;
 		// for (auto p : PR_paths_requiring_fences) {
